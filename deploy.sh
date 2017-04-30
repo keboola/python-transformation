bdf84e37fa8f7be9502882ec701648cf77ba71cb
Odin <odinuv@gmail.com>
2017-04-30 12:06:09 +0200

    fix: remove deprecated flag

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/python-transformation quay.io/keboola/python-transformation:$TRAVIS_TAG
 docker tag keboola/python-transformation quay.io/keboola/python-transformation:latest
 docker images
